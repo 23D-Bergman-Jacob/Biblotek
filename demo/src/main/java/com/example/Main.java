623d5d905140679d930a0b543ec0a4e71576b80e
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2026-05-12 12:06:49 +0200

    kan hämta alla böcker och lägga in i en lista

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -9,29 +9,50 @@ import kong.unirest.HttpResponse;
 import kong.unirest.UnirestException;
 
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
+        Test();
         String baseURL = "http://10.151.168.5:3110/"; 
-        HttpResponse<String>test;
+        HttpResponse<String>hämtadBok;
 
         Gson gson = new Gson();
         System.out.println("Böcker från tidningen");
         Böcker();
-        try {
-            test = Unirest.get(baseURL+"books/1").asString();            
-        } catch (UnirestException e) {
-            IO.println("kunde ej koppla upp " + e.getLocalizedMessage());
-            return;
+        int k = 0;
+        ArrayList<Bok> böcker = new ArrayList<Bok>();  
+        while (true) {
+            k++;
+            try {
+                hämtadBok = Unirest.get(baseURL+"books/" + k).asString();            
+            } catch (UnirestException e) {
+                IO.println("alla böcker hämtade" + e.getLocalizedMessage());
+                break;  
+            }
+            System.out.println(hämtadBok.getBody());
+            Bok exbok = gson.fromJson(hämtadBok.getBody(), Bok.class);
+            böcker.add(exbok);
+            System.out.println(böcker.get(k-1));
         }
-        System.out.println(test.getBody());
-        Bok exbok = gson.fromJson(test.getBody(), Bok.class);
-        System.out.println(exbok);
     }
 
     static void Böcker()
     {
         System.out.println("");
+    }
+
+    static String Test()
+    {
+            String baseURL = "http://10.151.168.5:3110/"; 
+            HttpResponse<String>test;
+            try {
+                test = Unirest.get(baseURL+"books/").asString();            
+            } catch (UnirestException e) {
+                IO.println("kunde ej koppla upp " + e.getLocalizedMessage());
+                return "nej";
+            }
+        return "ja";
     }
     
 }
